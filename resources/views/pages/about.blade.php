9a9f2fe6862f67babe2de21ae360a2f08ce747cb
DLDonaldson <livingdreams20@gmail.com>
2019-02-16 16:43:00 +0000

    This commit is to test pushing from a different computer

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,4 +3,5 @@
 @section('content')
     <h1> {{$title}} </h1>
     <p> This is the about page for an awesome app to give  your feedback </p>
+    <p> THis is more information about the app </p>
 @endsection
