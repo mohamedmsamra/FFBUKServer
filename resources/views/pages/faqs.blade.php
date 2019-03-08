dca40000b0f6a5b2626a43780ab32c491a40db15
DLDonaldson <livingdreams20@gmail.com>
2019-03-08 00:59:25 +0000

    Added generic marking and faqs pages Content still needs to be added

---
+++ resources/views/pages/faqs.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+	<style>
+	iframe {
+		height: 400px;
+		width: 715px;
+	}
+	</style>
+	<head>
+		<title>Fast Feedback UK (fFBUK)</title>
+		<link rel="shortcut icon" type="image/x-icon" href="../img/Tab_Icons/favicon.ico" />
+		<link rel="stylesheet" type="text/css" href="../css/generalstyle.css"/>
+		<script src="../js/jquery-3.2.1.min.js"></script>
+		<script src="../js/tabtoggle.js"></script>
+	</head>
+	<body>
+		<header>
+			<div>
+			<a href="../index.html">
+				<p style="float: left;"><img src="../img/Logo1.jpg" height="150px" width="150px"></p>
+				<br><br><br>
+				<b><p style="font-size:150%;">Fast FeedBack UK (fFBUK)</p></b>
+				<br>
+				<p style="color: blue"> &larr; Return to Homepage</p>
+			</a>
+			</div>    
+		</header>
+		<br><br><br>
+		
+		<section>
+		<h1>Tutorial Videos</h1>
+		<p>This series of videos will take you through all the functionality of the fFBUK software. Click on the icon in the top left of the video to skip to other videos in the playlist.</p>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PL3k51Gbd96lYLxLhES9asWtfWisC70hfV" frameborder="0" allowfullscreen>
+		</iframe>
+		</section>
+
+		<section>
+		<h1>Frequently Asked Questions</h1>
+		<strong>Why isn't the PDF loading?</strong>
+		<p>The software requires you use an up-to-date version of Google Chrome. To update or check the version of Google Chrome, follow the instructions <a href="https://support.google.com/chrome/answer/95414?co=GENIE.Platform%3DDesktop&hl=en">here</a>.</p>
+		<strong>Why isn't my template/feedback saving?</strong>
+		<p>Did any error messages appear when you clicked 'Save Form'? You must make sure all sections are completed.</p>
+		<p>If no error messages appear but the download does not start, check that you have not blocked downloads for the page. If you have blocked downloads, there will be a red icon on the right end of the URL bar. Click this to change the download settings to allow downloads from the page.</p>
+		</section>
+		
+		<section>
+		<h1>Further Queries</h1>
+		<p>If you have a question that isn't answered here, please contact us at&nbsp;<a href="mailto:ffbukcontact@gmail.com">ffbukcontact@gmail.com</a>.</p>
+		</section>		
+		
+		<br><br>
+	</body>
+</html>
