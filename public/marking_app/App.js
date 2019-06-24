5e2778ef5b74008fb3507fe4b0658791de071040
Vilmos Czeroczky <v.czeroczky@gmail.com>
2019-06-24 13:05:27 +0100

    Pass apps app state to new section modal

--- public/marking_app/App.js
+++ public/marking_app/App.js
@@ -86,7 +86,7 @@ class App extends React.Component {
                         </div>
                     }
                 </div>
-                <NewSectionModal />
+                <NewSectionModal data={this.state} />
             </div>
         );
     }
