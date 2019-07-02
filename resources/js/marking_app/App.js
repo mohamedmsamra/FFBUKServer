4cdc7a7dbd21ff7e386db38eec28cc75bf05f2cb
Vilmos Czeroczky <v.czeroczky@gmail.com>
2019-07-02 10:35:01 +0100

    Loading PDFs

--- resources/js/marking_app/App.js
+++ resources/js/marking_app/App.js
@@ -3,10 +3,30 @@ import PDFSide from './components/PDFSide';
 import MarkingSide from './components/MarkingSide';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pdfsSelected: [],
+            pdfPointer: -1
+        }
+        this.handlePdfsSelected = this.handlePdfsSelected.bind(this);
+    }
+
+    handlePdfsSelected(files) {
+        const pdfsSelected = [];
+        for (let i = 0; i < files.length; i++) {
+            pdfsSelected[i] = {name: files[i].name, url: URL.createObjectURL(files[i])};
+        }
+        this.setState({pdfsSelected: pdfsSelected, pdfPointer: 0});
+    }
+
     render() {
         return (
             <div className="row">
-                <PDFSide />
+                <PDFSide
+                    handlePdfsSelected={this.handlePdfsSelected}
+                    pdfsSelected={this.state.pdfsSelected}
+                    pdfPointer={this.state.pdfPointer} />
                 <MarkingSide />
             </div>
         )
--- resources/js/marking_app/components/PDFSide.js
+++ resources/js/marking_app/components/PDFSide.js
@@ -1,17 +1,32 @@
 import React from 'react';
 
 class PDFSide extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
+        const currentPdf = this.props.pdfsSelected[this.props.pdfPointer];
         return (
-            <div className="col">
-                <form>
-                    <div id="select-pdfs" class="input-group mb-3">
-                        <div class="custom-file">
-                            <input type="file" class="custom-file-input" name="pdfChoose" id="pdfChoose" accept=".pdf" multiple />
-                            <label class="custom-file-label" for="inputGroupFile01">Choose files to mark</label>
+            <div className="col" id="pdf-side">
+                {this.props.pdfPointer < 0 ?
+                    <form>
+                        <div id="select-pdfs" className="input-group mb-3">
+                            <div className="custom-file">
+                                <input onChange={(e) => this.props.handlePdfsSelected(e.target.files)} type="file" className="custom-file-input" name="pdfChoose" id="pdfChoose" accept=".pdf" multiple />
+                                <label className="custom-file-label" htmlFor="inputGroupFile01">Choose files to mark</label>
+                            </div>
                         </div>
+                    </form>
+                :
+                    <div>
+                        <p>{"Marking (" + (this.props.pdfPointer + 1) + "/" + this.props.pdfsSelected.length + "): " + currentPdf.name}</p>
+                        <embed
+                            src={currentPdf.url}
+                            type="application/pdf">
+                        </embed>
                     </div>
-                </form>
+                }
             </div>
         )
     }
