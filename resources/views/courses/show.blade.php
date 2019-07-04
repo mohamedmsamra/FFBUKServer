bfefc11d797f448c25fa5741a8fa247cda201c1a
Andreea Bila <andreeabila@yahoo.com>
2019-07-04 14:06:34 +0100

    fixing course picture issue started

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -4,7 +4,7 @@
     <div class="course-page" style="padding-left:50px">
         <a href="/courses" class="btn btn-link"> Go Back </a>
         <h1> {{$course->title}}</h1>
-        <img style= "width:10%, height:10%" src="{{ asset('stuff/default-1.jpg') }}">
+        <img class="course-img" src="{{ asset('stuff/default-1.jpg') }}">
         <div>
             {!! $course->body!!}
         </div>
