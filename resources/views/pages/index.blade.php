1933cac29f0ed624d5b0ce2a3aeda8a5d4bf0662
Andreea Bila <andreeabila@yahoo.com>
2019-07-22 14:55:42 +0100

    fixed resizing bug; verify email and reset password addes

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,6 +4,10 @@
 <div class="jumbotron text-center">
     <h1> {{$title}} </h1>
     <p> This is the index page for an awesome app to give  your feedback </p>
+    <button type="button" class="btn btn-primary">Assignment 1</button>
+    <button type="button" class="btn btn-primary">Assignment 2</button>
+    <a href="/survey1" class="btn btn-info">Pre-Workshop Survey</a>
+    <a href="/survey2" class="btn btn-info">Post-Workshop Survey</a>
 </div>    
 
 @endsection
